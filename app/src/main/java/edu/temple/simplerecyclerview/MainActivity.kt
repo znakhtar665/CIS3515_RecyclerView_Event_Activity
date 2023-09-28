ba8ef9aa87ebe446831f2d9d8d1bacc9eab95be5
Karl Morris <karl.morris@temple.edu>
2023-09-28 08:17:27 -0400

    Added textview for message display

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -15,9 +15,10 @@ class MainActivity : AppCompatActivity() {
         // Set up RecyclerView display responsibility
         val numbers = Array(100) {it + 1}
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
-        // TODO (Step 2: Define lambda)
+        // TODO (Step 2: Define lambda to modify displayTextView size)
 
         // Todo (Step 3: Pass lambda to adapter)
         recyclerView.adapter = NumberDisplayAdapter(numbers)
